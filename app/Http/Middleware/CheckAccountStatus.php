98a99eed1ceed9cfa48438f4af3cc50ad60a01da
shahd <mazouzchahd12@gmail.com>
2026-02-02 16:48:54 +0100

    feat: use the breez command in laravel

---
+++ app/Http/Middleware/CheckAccountStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAccountStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->is_active == false){
+            auth()->logout();
+            return redirect('/login')->withErrors(['email'=>'your account is inactive, contact the administrator']);
+        }
+        return $next($request);
+    }
+}
